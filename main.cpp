119c07b7c331adcc1ebe6f7f77f6fd5e71e91340
Edoardo W. Grappolini <ewgrappolini@gmail.com>
2021-06-08 19:32:41 +0200

    Breve traccia

--- main.cpp
+++ main.cpp
@@ -9,3 +9,14 @@ int main() {
     socota_db.aggiungiCorsa("Viale Morgagni", "Via di Santa Marta", 20210405, 10, "variabile");
     socota_db.aggiungiCorsa("Stazione campo di marte", "Stazione rifredi", 20210505, 20, "fissa");
 }
+
+/* Pt.1 In questo progetto, c'è una possibile implementazione di un database simulato, che registra le corse
+ * taxi fatte da una società cooperativa (socota).
+ * Ti hanno commissionato di migliorare il codice, dopo una lunga analisi decidi di migliorare il codice
+ * implementando il design pattern 'factory method' per permettere una migliore gestione dell'aggiunta e
+ * gestione delle corse. Contrassegnati con "TODO" ci sono alcuni suggerimenti sul da farsi.
+ *
+ * Pt.2 Il comune di Firenze decide che vuole, nel solito database, avere anche la traccia delle corse fatte
+ * dalle autovetture di cotafi. Cotafi però ha una gestione lievemente diversa delle corse! (vedi file "corsa cotafi").
+ * Come risolvi la questione?
+ * */
